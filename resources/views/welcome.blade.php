f6a8e7cfb3032bbd1a426be9d161e3dccf2e5869
antoine <antoine.guingand@wanadoo.fr>
2018-01-18 11:09:10 +0100

    Integrate beautiful Erwan mockup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,15 @@
 @extends('layout', ["bodyClass" => "bg-brand-brown"])
 
 @section('content')
-    <div class="w-screen h-screen font-sans font-bold text-white p-8">
-        <div class="container mx-auto flex flex-wrap">
-            <div class="w-full xl:w-1/3 lg:w-2/5 md:w-1/2 text-lg mb-8">
-                <header class="mb-8 md:mb-16">
-                    <h1 class="uppercase tracking-wider text-6xl">Code 16</h1>
-                    <h3 class="uppercase font-normal">Développement web et mobile</h3>
+    <div class="w-screen h-screen font-sans font-bold text-white tracking-wide p-8 sm:p-16">
+        <div class="container flex flex-wrap">
+            <div class="w-full xl:w-2/5 md:w-2/3 text-lg mb-12 md:pr-12">
+                <header class="mb-12">
+                    <h1 class="uppercase tracking-wider text-4xl bg-white text-brand-brown pb-6 pl-3 pt-2">Code 16</h1>
                 </header>
-                <section>
+                <div>
                     <p>
-                        <span>Philippe Lonchampt — </span>
+                        <span>Philippe Lonchampt&nbsp;&nbsp;</span>
                         <a class="font-normal" href="mailto:philippe@code16.fr">
                             <span class="hidden sm:inline">philippe@code16.fr</span>
                             <span class="sm:hidden">email</span>
@@ -18,17 +17,16 @@
                     </p>
                     <p>Rémi Collin</p>
                     <p>Antoine Guingand</p>
-                </section>
-                <section class="mt-8 font-normal">
+                </div>
+                <div class="mt-12 font-normal">
                     <p>SCM Gotham,</p>
                     <p>24 rue du Vieux marché aux Vins</p>
-                    <p>67000 Strasbourg, France</p>
-                </section>
-                <section class="mt-16 text-3xl">
-                    <p>Nouveau site à venir</p>
-                    <p class="font-normal">new website is coming</p>
-                </section>
-
+                    <p class="m-0">67000 Strasbourg, France</p>
+                </div>
+            </div>
+            <div class="w-full xl:w-1/5 lg:w-1/4 md:w-1/3 text-3xl">
+                <p>Nouveau site à venir</p>
+                <p class="font-normal">New website is coming</p>
             </div>
         </div>
     </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,5 +6,9 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
-        postCss: [ tailwindcss('./tailwind.js') ],
+        postCss: [ tailwindcss('./tailwind.js') ]
     });
+
+if(mix.inProduction()) {
+    mix.version();
+}
