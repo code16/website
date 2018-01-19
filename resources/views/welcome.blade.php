72cb4eda5701d14852134212d49d35df3fd836b8
Philippe Lonchampt <philippelonchampt@gmail.com>
2018-01-19 10:39:40 +0100

    Fix col width

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="w-screen h-screen font-sans font-bold text-white tracking-wide p-8 sm:p-16">
         <div class="container flex flex-wrap">
-            <div class="w-full xl:w-2/5 md:w-2/3 text-lg mb-12 md:pr-12">
+            <div class="w-full xl:w-2/5 lg:w-1/2 md:w-2/3 text-lg mb-12 md:pr-12">
                 <header class="mb-12">
                     <h1 class="uppercase tracking-wider text-4xl bg-white text-brand-brown pb-6 pl-3 pt-2">Code 16</h1>
                 </header>
