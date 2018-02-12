d077c6383be343211f330c0b51a67fe9338213ea
antoine <antoine.guingand@wanadoo.fr>
2018-02-12 19:40:03 +0100

    Update sticky titles

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,11 +1,13 @@
-<div class="mb-32 ml-48">
-    @if($title??false)
-        <div>
-            <h2 class="tracking-widest text-uppercase uppercase inline-block"
-                v-sticky-title="{ paddingTop:64, anchorMode:'start', relativeAnchorX:-100 }">
-                {{ $title }}
-            </h2>
-        </div>
-    @endif
-    <div class="{{ $large??false ? 'w-2/3' : 'w-2/3' }}">{{ $slot }}</div>
+<div class="pb-32">
+    <div class="relative">
+        @if($title??false)
+            <div class="pl-16">
+                <h2 class="tracking-widest text-uppercase uppercase inline-block"
+                    v-sticky-title:section="{ container:'.relative', paddingTop: 64, startOffset:-280, endOffset:20, relativeAnchorX: 40 }">
+                    {{ $title }}
+                </h2>
+            </div>
+        @endif
+        <div class="{{ $large??false ? 'w-2/3' : 'w-2/3' }} pl-48">{{ $slot }}</div>
+    </div>
 </div>
