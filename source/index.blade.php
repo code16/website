9f821febd55431d1fd111c392e8d2e0c8235b5ad
Philippe Lonchampt <philippelonchampt@gmail.com>
2019-05-15 17:57:29 +0200

    Sharp's url is https now

--- source/index.blade.php
+++ source/index.blade.php
@@ -63,7 +63,7 @@
                 Dans le même esprit, nous avons développé et nous maintenons plusieurs projets Open source, parmi lesquels&nbsp;:
             @endcomponent
             @component('_partials.paragraph')
-                <a href="http://sharp.code16.fr">Le framework de gestion de contenu Sharp</a>,
+                <a href="https://sharp.code16.fr/">Le framework de gestion de contenu Sharp</a>,
                 qui sert d'outil d'administration pour bon nombre de nos projets.
             @endcomponent
             @component('_partials.paragraph')
