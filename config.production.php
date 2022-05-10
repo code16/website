5c8412b6caad1e5311a88d23bc312e769a7b083c
philippe <philippe@code16.fr>
2022-05-10 20:34:58 +0200

    Fix prod script

--- config.production.php
+++ config.production.php
@@ -6,11 +6,31 @@ return [
     'production' => true,
     'baseUrl' => env('APP_URL', 'https://code16.fr'),
     'collections' => [
+        'projects' => [
+            'path' => 'p',
+            'extends' => '_layouts.project',
+            'section' => 'content',
+        ],
         'posts' => [
             ...$config['collections']['posts'],
             'filter' => function ($item) {
                 return !$item->draft;
             }
         ],
+    ],
+    'developers' => [
+        'philippe' => [
+            'name' => 'Philippe Lonchampt',
+            'avatar' => 'https://gravatar.com/avatar/' . md5('philippelonchampt@gmail.com') . '?d=retro',
+            'twitter' => 'https://twitter.com/dvlpp',
+        ],
+        'arnaud' => [
+            'name' => 'Arnaud Becher',
+            'avatar' => 'https://gravatar.com/avatar/' . md5('arnaud.becher@gmail.com') . '?d=retro',
+        ],
+        'antoine' => [
+            'name' => 'Antoine Guingand',
+            'avatar' => 'https://gravatar.com/avatar/' . md5('guingand.antoine@gmail.com') . '?d=retro',
+        ]
     ],
 ];
